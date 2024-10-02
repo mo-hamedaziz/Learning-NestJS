cec0662c28fc53702be6d5f71a5601e16cbfa858
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-10-03 00:11:54 +0100

    Employee resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, ServiceModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
